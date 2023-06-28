763292b73a66b97496ad3ace134168da0d89fcf0
Jejomar Batitis <jejomar.batitis@ideascience.com>
2023-06-29 09:38:52 +1000

    DEVELOP: added angular material:

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  exports: [MatFormFieldModule, MatInputModule],
 })
-export class SharedModule { }
+export class SharedModule {}
